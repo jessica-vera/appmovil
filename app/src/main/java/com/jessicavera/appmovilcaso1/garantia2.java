94b54477b7ca5be028fb22fc1149e2f5be90df5d
Wilson <wilsonvazquezq@gmail.com>
2021-12-05 16:54:20 -0500

    Login para garantia

---
+++ app/src/main/java/com/jessicavera/appmovilcaso1/garantia2.java
@@ -0,0 +1,14 @@
+package com.jessicavera.appmovilcaso1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class garantia2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_garantia2);
+    }
+}
