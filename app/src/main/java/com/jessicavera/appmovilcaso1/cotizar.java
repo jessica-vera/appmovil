2e876eb448f151ac28b717defd0873842a14dc2c
97jessicavera <veramogrovejo@gmail.com>
2021-12-05 22:39:30 -0600

    cambio de interfaz cotizar

--- app/src/main/java/com/jessicavera/appmovilcaso1/cotizar.java
+++ app/src/main/java/com/jessicavera/appmovilcaso1/cotizar.java
@@ -22,7 +22,7 @@ public class cotizar extends AppCompatActivity {
         enviarcotizacion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent env = new Intent(cotizar.this, detallevehiculo.class);
+                Intent env = new Intent(cotizar.this, mensaje.class);
                 startActivity(env);
             }
         });
--- app/src/main/java/com/jessicavera/appmovilcaso1/detallevehiculo.java
+++ app/src/main/java/com/jessicavera/appmovilcaso1/detallevehiculo.java
@@ -9,13 +9,13 @@ import android.widget.Button;
 
 public class detallevehiculo extends AppCompatActivity {
 
-    Button detalle ;
+    //Button detalle ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detallevehiculo);
 
-        detalle = (Button)findViewById(R.id.bnt_cotizarvehiculo);
+       /* detalle = (Button)findViewById(R.id.bnt_cotizarvehiculo);
 
         detalle.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,6 +23,6 @@ public class detallevehiculo extends AppCompatActivity {
                 Intent i = new Intent(detallevehiculo.this, mensaje.class);
                 startActivity(i);
             }
-        });
+        });*/
     }
 }
